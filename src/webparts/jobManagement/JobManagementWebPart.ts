20e0903e8e85a941bf3b36fa1443baf1f94a75aa
ArunRaj2312 <arunvijay1043534@gmail.com>
2023-06-19 12:23:13 +0530

    first change

--- src/webparts/jobManagement/JobManagementWebPart.ts
+++ src/webparts/jobManagement/JobManagementWebPart.ts
@@ -35,7 +35,8 @@ export default class JobManagementWebPart extends BaseClientSideWebPart<IJobMana
         isDarkTheme: this._isDarkTheme,
         environmentMessage: this._environmentMessage,
         hasTeamsContext: !!this.context.sdks.microsoftTeams,
-        userDisplayName: this.context.pageContext.user.displayName
+        userDisplayName: this.context.pageContext.user.displayName,
+        context:this.context
       }
     );
 
---
+++ src/webparts/jobManagement/components/MainComponent.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+const MainCoimponent=(props:any)=>{
+    console.log('props',props.context);
+    
+    return(
+        <div>
+            Main
+        </div>
+    )
+}
+export default MainCoimponent
