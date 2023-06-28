316c1f512fe075084c70ecadb2253fc719eaf683
aravindh203 <aravindhkumar218@gmail.com>
2023-06-28 14:58:08 +0530

    new gap changes

--- src/webparts/jobManagement/components/Provider/providerFrom.tsx
+++ src/webparts/jobManagement/components/Provider/providerFrom.tsx
@@ -13,7 +13,7 @@ interface IProviderAdd{
     SecondAddress:string;
     Nok:boolean;
     NokName:string;
-    NokPhoneNo:number;
+    NokPhoneNo:string;
     Files:any;
 }
 
@@ -26,6 +26,7 @@ const ProviderAddForm = (props:any):JSX.Element =>{
             }
         }
     }
+
     const [error,setError] = useState<string>('')
     const [phoneNum,setPhoneNum]=useState([])    
     const [data,setData] = useState<IProviderAdd>({
@@ -44,48 +45,31 @@ const ProviderAddForm = (props:any):JSX.Element =>{
         let Data={...data}
         Data[key]=value
         setData(Data)
-        // if(event.target.name==='Provider Name'){
-        //     setData({...data,Name:event.target.value})
-        // }
-        // else if(event.target.name==='Phone No'){
-        //     setData({...data,PhoneNo:event.target.value})
-        // }
-        // else if(event.target.name==='Email'){
-        //     setData({...data,Email:event.target.value})
-        // }
-        // else if(event.target.name==='Contact Address'){ 
-        //     setData({...data,FirstAddress:event.target.value})
-        // }
-        // else if(event.target.name==='Second Address'){
-        //     setData({...data,SecondAddress:event.target.value})
-        // }
-        // else if(event.target.name==='Nok'){
-        //     setData({...data,Nok:event.target.checked})
-        // }
-        // else if(event.target.name==='Nok Name'){
-        //     setData({...data,NokName:event.target.value})
-        // }
-        // else if(event.target.name==='file'){
-        //     setData({...data,Files:event.target.files})
-        // }
-        // else{
-        //     setData({...data,NokPhoneNo:event.target.value})
-        // }
+       
     }
+
     const fileUpload=(datas)=>{
+
         let filesData=[];
         for(let i=0;i<datas.length;i++){
             filesData.push(datas[i])
         } 
         
         setData({...data,Files:filesData})
+
     }
+
     const handleFileClose=(index)=>{
+
         let deletedDoc=[...data.Files]
         deletedDoc.splice(index,1)
+
         setData({...data,Files:deletedDoc})
+
     }
+
     const getPhoneNovalidation=async()=>{
+
         await sp.web.lists.getByTitle(props.list.listName).items.select("PhoneNo").get().then((item)=>{
             
            let phoneNum=[];
@@ -93,11 +77,12 @@ const ProviderAddForm = (props:any):JSX.Element =>{
                 let mobileNumber = value.PhoneNo ? value.PhoneNo:''
                 phoneNum.push(mobileNumber)
            })
+
            setPhoneNum([...phoneNum])
         }).catch((error)=>errorFunction(error,"getPhonoNo"))
+
     }
     const validation = (btnVal:string):boolean =>{
-               // return false;
 
         let isAllValueFilled=true;
         let emailvalidation=/^([A-Za-z0-9_.])+\@([g][m][a][i][l])+\.([c][o][m])+$/;
@@ -105,6 +90,7 @@ const ProviderAddForm = (props:any):JSX.Element =>{
         let phoneNoValidation=[...phoneNum].every(value=>{            
             return value !== data.PhoneNo
         })
+
         if(!data.Name){
             setError('please fill name')
             isAllValueFilled = false;
@@ -143,12 +129,13 @@ const ProviderAddForm = (props:any):JSX.Element =>{
             setError('')
             isAllValueFilled = true;
         }
+
         return isAllValueFilled;
+
     }
-    useEffect(()=>{
-        getPhoneNovalidation()
-    },[])
+    
     const handleSubmit = async (btnVal:string) =>{
+
         var submitAuthetication = validation(btnVal);
     
         let newJson={
@@ -173,18 +160,15 @@ const ProviderAddForm = (props:any):JSX.Element =>{
                 props.setChange({...props.change,provider:true,isSpinner:false})
             })
         }
+        
     }
 
-    async function createFolder(ItemID)
-    {
-        //await sp.web.lists.getByTitle('ProviderAttachment').rootFolder.folders.add(ItemID)
+    async function createFolder(ItemID){
+
         await sp.web.rootFolder.folders.getByName(props.list.libraryName).folders.addUsingPath(ItemID.toString(),true)
-        .then(async (result)=> 
-        {
-            // Create a file inside the newly created folder
+        .then(async (result)=> {
             for(let i=0;i<data.Files.length;i++){
                 sp.web.getFolderByServerRelativePath(result.data.ServerRelativeUrl).files.addUsingPath(data.Files[i].name, data.Files[i], { Overwrite: true })
-                //await result.folder.files.add(data.Files[i].name, data.Files[i])
                 .then(async (file) => {
                     await errorFunction('File created successfully:', file);
                 })
@@ -192,17 +176,20 @@ const ProviderAddForm = (props:any):JSX.Element =>{
                     await errorFunction('Error creating file:', error);
                 });
             }
-            
         })
         .catch(error => {
             errorFunction('Error creating folder:', error);
         });
 
     }
+
     const errorFunction=(name:string,error:any)=>{
         console.log(error,name);
-        
     }
+
+    useEffect(()=>{
+        getPhoneNovalidation()
+    },[])
     
     return(
         <div className={styles.contain}>
@@ -241,7 +228,7 @@ const ProviderAddForm = (props:any):JSX.Element =>{
                                 <TextField value={data.NokName} label='Nok Name'styles={text} name='Nok Name' onChange={(event,text)=>handleInputValue("NokName",text)} disabled={false}/>
                             </div>
                             <div>
-                                <TextField value={data.NokPhoneNo ? data.NokPhoneNo.toString():''} styles={text} label='Nok Phone No' name='Nok Phone No' type='number' onChange={(event,text)=>handleInputValue("NokPhoneNo",text)} disabled={false}/>
+                                <TextField value={data.NokPhoneNo ? data.NokPhoneNo.toString():''} styles={text} label='Nok Phone No' name='Nok Phone No' onChange={(event,text)=>handleInputValue("NokPhoneNo",text)} disabled={false}/>
                             </div>
                         </div>
                         )
